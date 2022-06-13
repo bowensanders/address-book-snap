7f9ca0f01adf089f6b4a8ed0b0f293b7e93f6119
bowensanders <bowensanders@gmail.com>
2022-06-13 16:25:44 -0700

    Changes for testing purposes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
 wallet.registerRpcMessageHandler(async (originString, requestObject) => {
+  let state = await wallet.request({
+    method: 'snap_manageState',
+    params: ['get'],
+  });
+
+  if (!state) {
+    state = { book: [] };
+    // initialize state if empty and set default data
+    await wallet.request({
+      method: 'snap_manageState',
+      params: ['update', state],
+    });
+  }
+
   switch (requestObject.method) {
+    case 'storeAddress':
+      state.book.push({
+        name: requestObject.nameToStore,
+        address: requestObject.addressToStore,
+      });
+      await wallet.request({
+        method: 'snap_manageState',
+        params: ['update', state],
+      });
+      return true;
+    case 'retrieveAddresses':
+      return state.book;
     case 'hello':
+      let address_book = state.book
+        .map(function (item) {
+          return `${item.name}: ${item.address}`;
+        })
+        .join('\n');
       return wallet.request({
         method: 'snap_confirm',
         params: [
           {
             prompt: `Hello, ${originString}!`,
-            description:
-              'This custom confirmation is just for display purposes.',
-            textAreaContent:
-              'But you can edit the snap source code to make it do something, if you want to!',
+            description: 'Address book:',
+            textAreaContent: address_book,
           },
         ],
+      });
+    case 'clearTestData':
+      await wallet.request({
+        method: 'snap_manageState',
+        params: ['clear'],
       });
     default:
       throw new Error('Method not found.');
